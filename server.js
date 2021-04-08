1fc1a3cde4049b2f539d61e4317947a8f191ab0d
kinz-up <berksmile@gmail.com>
2021-04-08 21:42:05 +0900

    feat: mod cors rule for socket.io

--- server.js
+++ server.js
@@ -3,6 +3,36 @@ const socket = require("socket.io");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const io = socket(server);
+const io = socket(server, { cors: { origin: "*" } });
 
-app.use('/', route)
+io.sockets.on("connection", function (socket) {
+  socket.on("newUser", function (name) {
+    console.log(name, +" 님이 접속하였습니다.");
+
+    socket.name = name;
+
+    io.sockets.emit("update", {
+      type: "connect",
+      name: "SERVER",
+      message: name + " 님이 접속하였습니다.",
+    });
+  });
+
+  socket.on("message", function (data) {
+    data.name = socket.name;
+    console.log(data);
+
+    socket.broadcast.emit("update", data);
+  });
+
+  socket.on("disconnect", function () {
+    console.log(socket.name + " 님이 나가셨습니다.");
+
+    socket.broadcast.emit("update", {
+      type: "disconnect",
+      name: "SERVER",
+      message: socket.name + " 님이 나가셨습니다.",
+    });
+  });
+});
+server.listen(8080, () => console.log("The server has been launched"));
